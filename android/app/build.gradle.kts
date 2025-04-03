fb0ed4f1c09581169321370c33735128154b2d5a
macapagal-marc-lawrence <macapagalmarck@gmail.com>
2025-04-03 16:39:33 +0800

    commit by marc

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,6 +9,7 @@ android {
     namespace = "com.devops.ordering_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -29,22 +30,22 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file(System.getenv("CM_KEYSTORE_PATH") ?: rootProject.file("android/app/keystore.jks"))
-            storePassword = System.getenv("KEYSTORE_PASSWORD") ?: ""
-            keyAlias = System.getenv("KEY_ALIAS") ?: ""
-            keyPassword = System.getenv("KEY_PASSWORD") ?: ""
+            storeFile = file(System.getenv("CM_KEYSTORE_PATH"))
+            storePassword = System.getenv("CM_KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("CM_KEY_ALIAS")
+            keyPassword = System.getenv("CM_KEY_PASSWORD")
         }
     }
-
 
     buildTypes {
         release {
-
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("release")
-            )
         }
     }
 }
+
 
 flutter {
     source = "../.."
