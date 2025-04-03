6a2f3f0d8797096d4e53bec896da89d411bc9bfa
macapagal-marc-lawrence <macapagalmarck@gmail.com>
2025-04-03 16:54:22 +0800

    commit by marc

--- android/app/src/main/java/com/example/ordering_app/MainActivity.kt
+++ android/app/src/main/java/com/example/ordering_app/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.devops.ordering_app;
 
-import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity
 
-public class MainActivity extends FlutterActivity {
-}
+class MainActivity : FlutterActivity()
